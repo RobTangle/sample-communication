06babf51c1979f953fee5e56cc8e8c1aebb7d479
Robert0101 <rob.tangled@gmail.com>
2023-02-01 22:35:51 -0300

    setup basic logic

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserEvent } from './create-user.event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  handleUserCreated(data: CreateUserEvent) {
+    console.log(
+      '🚀 ~ file: COMMUNICATIONS app.service.ts:11 ~ AppService ~ handleUserCreated ~ data',
+      data,
+    );
+    //todo: Email the user...
+    console.log(
+      `I'm doing something with the user email ${data.email}that the communication microservices recived....`,
+    );
   }
 }
